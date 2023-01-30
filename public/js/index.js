853df8dd42de47cf6ec4757e4f59a52e6059cb7b
Aditya Sharma <59206491+ad1tyaSharma@users.noreply.github.com>
2023-01-30 11:32:14 +0530

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -11,7 +11,7 @@ const emailForm = document.querySelector("#emailForm");
 
 const toast = document.querySelector(".toast");
 
-const baseURL = "https://busy-erin-tadpole-wig.cyclic.app";
+const baseURL = "https://cloudshare-api.onrender.com";
 const uploadURL = `${baseURL}/api/files`;
 const emailURL = `${baseURL}/api/files/send`;
 
